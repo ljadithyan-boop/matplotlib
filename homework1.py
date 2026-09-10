4d84fc534985767049ca61f68d8ffc2c2c448673
adithyan lj <ljadithyan@gmail.com>
2026-09-10 15:06:25 +0530

    homework of day 1

---
+++ homework1.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+months=["January", "February", "March", "April"]
+product_a=[200,250,300,350]
+product_b=[180,220,260,310]
+fig, ax = plt.subplots()
+ax.plot(months,product_a,marker="o",color="green",linestyle="-")
+ax.plot(months,product_b,marker="s",color="red",linestyle="--")
+plt.xlabel("month")
+plt.ylabel("sales")
+plt.plot()
+plt.show()
+
+
+print("---------part2----------")
+import matplotlib.pyplot as plt
+fig,ax=plt.subplots(2,1,sharex=True)
+plt.axes[0].plot([product_a])
+plt.axes[1].plot([product_b])
+plt.show()
+
+
